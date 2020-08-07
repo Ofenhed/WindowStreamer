227af152e8880e3b11afa3b8a0d73b7d2404b4c6
Marcus Ofenhed <marcus@conditionraise.se>
2020-08-07 10:46:53 +0200

    Initial commit. Spy++ like functionality and screenshots working. Streaming not implemented.

---
+++ WindowStreamer/WindowFinder.Designer.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace WindowStreamer
+{
+    partial class WindowFinder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+        {
+            this.previewBox = new System.Windows.Forms.PictureBox();
+            this.gotWindowPanel = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.sizeHeight = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sizeWidth = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.portLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.previewBox)).BeginInit();
+            this.gotWindowPanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sizeHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sizeWidth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // previewBox
+            // 
+            this.previewBox.AccessibleName = "";
+            this.previewBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.previewBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.previewBox.Location = new System.Drawing.Point(3, 3);
+            this.previewBox.Name = "previewBox";
+            this.previewBox.Size = new System.Drawing.Size(722, 302);
+            this.previewBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.previewBox.TabIndex = 0;
+            this.previewBox.TabStop = false;
+            // 
+            // gotWindowPanel
+            // 
+            this.gotWindowPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gotWindowPanel.Controls.Add(this.panel1);
+            this.gotWindowPanel.Controls.Add(this.previewBox);
+            this.gotWindowPanel.Location = new System.Drawing.Point(12, 12);
+            this.gotWindowPanel.Name = "gotWindowPanel";
+            this.gotWindowPanel.Size = new System.Drawing.Size(728, 447);
+            this.gotWindowPanel.TabIndex = 1;
+            this.gotWindowPanel.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.sizeHeight);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.sizeWidth);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.portLabel);
+            this.panel1.Location = new System.Drawing.Point(3, 311);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(722, 133);
+            this.panel1.TabIndex = 3;
+            // 
+            // sizeHeight
+            // 
+            this.sizeHeight.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.sizeHeight.Location = new System.Drawing.Point(109, 16);
+            this.sizeHeight.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.sizeHeight.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.sizeHeight.Name = "sizeHeight";
+            this.sizeHeight.Size = new System.Drawing.Size(64, 20);
+            this.sizeHeight.TabIndex = 5;
+            this.sizeHeight.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.sizeHeight.ValueChanged += new System.EventHandler(this.sizeHeight_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Size";
+            // 
+            // sizeWidth
+            // 
+            this.sizeWidth.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.sizeWidth.Location = new System.Drawing.Point(39, 16);
+            this.sizeWidth.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.sizeWidth.Name = "sizeWidth";
+            this.sizeWidth.Size = new System.Drawing.Size(64, 20);
+            this.sizeWidth.TabIndex = 3;
+            this.sizeWidth.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.sizeWidth.ValueChanged += new System.EventHandler(this.sizeWidth_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Listening on port";
+            // 
+            // portLabel
+            // 
+            this.portLabel.AutoSize = true;
+            this.portLabel.Location = new System.Drawing.Point(94, 0);
+            this.portLabel.Name = "portLabel";
+            this.portLabel.Size = new System.Drawing.Size(35, 13);
+            this.portLabel.TabIndex = 2;
+            this.portLabel.Text = "label2";
+            // 
+            // WindowFinder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 471);
+            this.Controls.Add(this.gotWindowPanel);
+            this.Name = "WindowFinder";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.previewBox)).EndInit();
+            this.gotWindowPanel.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sizeHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sizeWidth)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PictureBox previewBox;
+        private Panel gotWindowPanel;
+        private Label label1;
+        private Label portLabel;
+        private Panel panel1;
+        private Label label2;
+        private NumericUpDown sizeWidth;
+        private NumericUpDown sizeHeight;
+    }
+}
+
---
+++ WindowStreamer/WindowFinder.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace WindowStreamer
+{
+
+    public partial class WindowFinder : Form
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern IntPtr SetCapture(IntPtr hWnd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool ReleaseCapture();
+
+        [DllImport("user32.dll")]
+        static extern IntPtr WindowFromPoint(System.Drawing.Point p);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetParent(IntPtr hwnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool PrintWindow(IntPtr hwnd, IntPtr hDC, uint nFlags);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll", EntryPoint = "SetWindowsHookEx", SetLastError = true)]
+        static extern IntPtr SetWindowsHookEx(int idHook, Delegate lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;        // x position of upper-left corner
+            public int Top;         // y position of upper-left corner
+            public int Right;       // x position of lower-right corner
+            public int Bottom;      // y position of lower-right corner
+        }
+
+        private IntPtr activeWindow;
+        private RECT? posBefore;
+        private Thread workerThread;
+        private Action resetFunction;
+        private bool updateSize = false;
+
+        public WindowFinder()
+        {
+            InitializeComponent();
+            this.MouseDown += new MouseEventHandler(this.formMouseDown);
+            this.MouseUp += new MouseEventHandler(this.formMouseUp);
+            this.previewBox.MouseDown += new MouseEventHandler(this.formMouseDown);
+            // this.MouseCaptureChanged += new Mous
+
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            if (posBefore.HasValue)
+            {
+                var value = posBefore.Value;
+                MoveWindow(activeWindow, value.Left, value.Top, value.Right - value.Left, value.Bottom - value.Top, false);
+            }
+            base.OnClosed(e);
+        }
+
+        public void formMouseDown(Object sender, MouseEventArgs e)
+        {
+            WindowFinder.SetCapture(this.Handle);
+        }
+
+        public bool streamHwnd(IntPtr hwnd)
+        {
+            activeWindow = hwnd;
+            var hwnd2 = GetParent(activeWindow);
+            if (hwnd2 != IntPtr.Zero) { activeWindow = hwnd2; }
+            if (activeWindow != IntPtr.Zero)
+            {
+                workerThread = new Thread(delegate ()
+                {
+                    posBefore = null;
+                    resetFunction = (delegate
+                    {
+                        workerThread.Abort();
+                        if (posBefore.HasValue)
+                        {
+                            var value = posBefore.Value;
+                            MoveWindow(activeWindow, value.Left, value.Top, value.Right - value.Left, value.Bottom - value.Top, false);
+                            if (!IsDisposed)
+                            {
+                                previewBox.Image = null;
+                                gotWindowPanel.Visible = false;
+                            }
+                        }
+                        resetFunction = null;
+                    });
+                    Bitmap[] bmp = { new Bitmap(1, 1), new Bitmap(1, 1) };
+                    Graphics[] memoryGraph = { Graphics.FromImage(bmp[0]), Graphics.FromImage(bmp[1]) };
+                    var idx = 0;
+
+                    Action doResetFunction = delegate
+                    {
+                        if (resetFunction != null)
+                        {
+                            _ = Invoke(resetFunction);
+                        }
+                    };
+
+                    while (true)
+                    {
+                        var rect = new RECT();
+                        var got_size = GetWindowRect(activeWindow, out rect);
+                        if (!got_size)
+                        {
+                            doResetFunction();
+                            break;
+                        }
+                        if (bmp[idx].Width != rect.Right - rect.Left || bmp[idx].Height != rect.Bottom - rect.Top)
+                        {
+                            bmp[idx] = new Bitmap(rect.Right - rect.Left, rect.Bottom - rect.Top);
+                            memoryGraph[idx] = Graphics.FromImage(bmp[idx]);
+                        }
+                        var screen = SystemInformation.VirtualScreen;
+                        sizeWidth.Maximum = screen.Width;
+                        sizeHeight.Maximum = screen.Height;
+                        if (posBefore == null)
+                        {
+                            posBefore = rect;
+                            sizeWidth.Value = bmp[idx].Width;
+                            sizeHeight.Value = bmp[idx].Height;
+                            updateSize = true;
+                        }
+                        if (updateSize)
+                        {
+                            updateSize = false;
+                            previewBox.Image = null;
+                            MoveWindow(activeWindow, screen.Width, screen.Height, (int)sizeWidth.Value, (int)sizeHeight.Value, false);
+                            continue;
+                        }
+                        var ptr = memoryGraph[idx].GetHdc();
+                        var printed = PrintWindow(activeWindow, ptr, 0);
+                        memoryGraph[idx].ReleaseHdc();
+                        if (printed)
+                        {
+                            var image = bmp[idx];
+                            try
+                            {
+                                _ = Invoke((Action)(delegate
+                                {
+                                    if (IsDisposed)
+                                    {
+                                        return;
+                                    }
+                                    sizeWidth.Value = image.Width;
+                                    sizeHeight.Value = image.Height;
+                                    gotWindowPanel.Visible = true;
+                                    if (previewBox.Image == null)
+                                    {
+                                        idx ^= 1;
+                                        previewBox.Image = image;
+                                    }
+                                }));
+                            }
+                            catch (System.InvalidOperationException)
+                            {
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            doResetFunction();
+                            break;
+                        }
+                        Thread.Sleep(1000 / 30);
+                    }
+                });
+                workerThread.Priority = ThreadPriority.Lowest;
+                workerThread.Start();
+                return true;
+            }
+            return false;
+        }
+
+        public void formMouseUp(Object sender, MouseEventArgs e)
+        {
+            WindowFinder.ReleaseCapture();
+
+            var hwnd = WindowFromPoint(System.Windows.Forms.Control.MousePosition);
+            if (hwnd != Handle)
+            {
+                streamHwnd(hwnd);
+            }
+        }
+
+        private void sizeWidth_ValueChanged(object sender, EventArgs e)
+        {
+            updateSize = true;
+        }
+
+        private void sizeHeight_ValueChanged(object sender, EventArgs e)
+        {
+            updateSize = true;
+        }
+    }
+}
